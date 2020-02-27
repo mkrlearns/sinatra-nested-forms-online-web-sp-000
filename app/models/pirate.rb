c9eef1f19e9bb8cad9c235207bfc9dc9effc0792
ryan m <contact@ryanmeek.com>
2020-02-27 21:03:06 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
+Learn more or give us feedback
 class Pirate
+  attr_accessor :name, :height, :weight
+  @@all = []
+
+  def initialize(args)
+    @name = args[:name]
+    @height = args[:height]
+    @weight = args[:weight]
+    @@all << self
+  end
+
+  def self.all() @@all end
+
 end
